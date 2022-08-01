06590b7a098a320eb7b0b18274add40b941f496b
Jody-septiawan <jody.septiawan5@gmail.com>
2022-08-01 02:28:27 -0700

    Routing

--- main.go
+++ main.go
@@ -14,12 +14,15 @@ func main() {
 	// On http (API)
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello World"))
-	}).Methods("GET")
+	r.HandleFunc("/", helloWorld ).Methods("GET")
 
 	fmt.Println("server running localhost:5000")
 	http.ListenAndServe("localhost:5000", r)
+}
+
+
+func helloWorld(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello World"))
 }
