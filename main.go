3a157b92f7deb197513e6961f90bad037108f12d
Jody-septiawan <jody.septiawan5@gmail.com>
2022-08-01 02:11:39 -0700

    Make Hello World

--- main.go
+++ main.go
@@ -1,22 +1,3 @@
-# Make Hello World
-
-### 1. Initializing project
-
-```bash
-go mod init _project_name_
-```
-
-### 2. Install gorilla/mux
-
-```bash
-go get -u github.com/gorilla/mux
-```
-
-Package `gorilla/mux` implements a request router and dispatcher for matching incoming requests to their respective handler.
-
-### 3. Create `main.go` file and write this below code to print 'Hello wolrd'
-
-```go
 package main
 
 import (
@@ -42,4 +23,3 @@ func main() {
 	fmt.Println("server running localhost:5000")
 	http.ListenAndServe("localhost:5000", r)
 }
-```
